61c5d27f4f2ad331c553a78526ffa416c6fdc404
AYUSHGARJE <105366673+AYUSHGARJE@users.noreply.github.com>
2022-05-16 18:57:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var trex ,trex_running;
+var ground,groundImage
+function preload(){
+  trex_running=loadAnimation{"trex1.png",trex3.png,trex4.png}
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex.createprite
+}
+
+function draw(){
+  background("black")
+  drawsprite()
+
+}
